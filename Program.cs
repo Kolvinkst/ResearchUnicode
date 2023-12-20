26acc6aa0774c583e4c042bbda98d6594b6cf635
Kolvinkst <einbenutzer1998@gmail.com>
2023-12-20 12:06:32 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ResearchUnicode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("eureka.txt", "Eureka!");
+            byte[] eurekaBytes = File.ReadAllBytes("eureka.txt");
+            foreach (byte b in eurekaBytes)
+                Console.Write("{0}", b);
+            Console.WriteLine(Encoding.UTF8.GetString(eurekaBytes));
+            char key = Console.ReadKey().KeyChar;
+        }
+    }
+}
